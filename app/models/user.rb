1996d6e46c450e44c0d99fbb13fb7788b4ba1fb5
vagrant <vagrant@localhost.localdomain>
2020-06-12 10:30:52 +0000

    Finish フォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,17 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_tweets, through: :favorites, source: :tweet, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  # 自分がフォローしているユーザーとの関連
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :followings, through: :active_relationships, source: :follower
+
+  # 自分がフォローされるユーザーとの関連
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :passive_relationships, source: :following
+
+  def followed_by?(user)
+  	passive_relationships.find_by(following_id: user.id).present?
+  end
+
 end
